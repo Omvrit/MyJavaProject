460ae9769069e00ec84fd4c7a1d39fe55214118e
omvrit <124257344+Omvrit@users.noreply.github.com>
2025-05-15 23:47:48 +0530

    My java project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+        var time = date.getTime();
+        System.out.println(time);
+        System.out.println("This is me om ommvrit");
+
+
+    }
+}
